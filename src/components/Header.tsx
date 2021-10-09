97a14f60ca7944a75f902d01b1a02d800bfe2239
Djonata Webber <wdjonata@gmail.com>
2021-10-09 18:09:17 -0300

    concepts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,13 @@ interface HeaderProps {
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
-  // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+
+
+  if (tasksCounter == 1) {
+    var tasksCounterText = 'tarefa'
+  }else {
+    var tasksCounterText = 'tarefas'
+  }
   
   return (
     <View style={styles.container}>
@@ -17,7 +23,7 @@ export function Header({ tasksCounter }: HeaderProps) {
       
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        {<Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>}
       </View>
     </View>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,33 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState,data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+
+    const updateTasks = tasks.map( task => ({ ...task }))
+
+    const item = updateTasks.find(element => (element.id == id))
+    
+    if (!item) {
+      return
+    }
+
+    item.done = !item.done
+
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
